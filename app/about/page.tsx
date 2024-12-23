92803f9d7508413ee16e4a08b783f4c42b6e0de7
Dawit yang <didekdnlt1996@gmail.com>
2024-12-23 23:33:48 +0900

    feat : about, post 페이지 생성 및 Nav 링크 연동

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>about 페이지</p>
+    </div>
+  );
+}
---
+++ app/post/page.tsx
@@ -0,0 +1,8 @@
+export default function Post() {
+  return (
+    <div>
+      <h1>Post Page</h1>
+      <p>포스트 페이지</p>
+    </div>
+  );
+}
