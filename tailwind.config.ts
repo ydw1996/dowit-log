314b02699cece98d34b0b86496fb49f3925b7a2d
Dawit yang <didekdnlt1996@gmail.com>
2025-01-15 00:13:25 +0900

    fix: Tailwind CSS 설정 및 프로세스 개선
    - `theme()` 함수 사용 제거 및 다크 모드 지원 수정
    - 잘못된 `@apply` 사용 수정 및 사용자 정의 클래스 추가
    - 타이포그래피 플러그인 및 Tailwind 설정 문제 해결

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,46 +1,53 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: {
-          '01': '#2E42D1', // primary-01
-          '02': '#244EF7', // primary-02
+          "01": "#2E42D1", // primary-01
+          "02": "#244EF7", // primary-02
         },
         basic: {
-          white: '#FFFFFF',
-          black: '#030303',
+          white: "#FFFFFF",
+          black: "#030303",
         },
         bg: {
-          white: '#F7F9FB',
-          dark: '#1E2022',
+          light: "#F7F9FB",
+          dark: "#1E2022",
         },
         gray: {
-          700: '#3A3A3A',
-          600: '#767676',
-          500: '#999999',
-          400: '#CCCCCC',
-          300: '#DDDDDD',
-          200: '#EDEDED',
-          100: '#F6F6F6',
+          700: "#3A3A3A",
+          600: "#767676",
+          500: "#999999",
+          400: "#CCCCCC",
+          300: "#DDDDDD",
+          200: "#EDEDED",
+          100: "#F6F6F6",
         },
       },
       fontFamily: {
-        pretendard: ['Pretendard', 'Arial', 'sans-serif'],
+        pretendard: ["Pretendard", "Arial", "sans-serif"],
       },
       fontSize: {
-        display: ['84px', { lineHeight: '100%' }],
-        h1: ['54px', { lineHeight: '100%' }],
-        h2: ['40px', { lineHeight: '100%' }],
-        h3: ['32px', { lineHeight: '100%' }],
-        h4: ['24px', { lineHeight: '100%' }],
-        h5: ['20px', { lineHeight: '100%' }],
-        h6: ['16px', { lineHeight: '100%' }],
+        "display-lg": ["84px", { lineHeight: "120%" }],
+        "heading-xl": ["54px", { lineHeight: "120%" }],
+        "heading-lg": ["40px", { lineHeight: "120%" }],
+        "heading-md": ["32px", { lineHeight: "120%" }],
+        "heading-sm": ["24px", { lineHeight: "120%" }],
+        "body-lg": ["20px", { lineHeight: "150%" }],
+        "body-md": ["16px", { lineHeight: "150%" }],
+        "body-sm": ["14px", { lineHeight: "150%" }],
+      },
+      spacing: {
+        "section-y": "60px",
+        "section-sm-y": "40px",
+        "card-padding": "20px",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/typography'), // 이 부분 추가
+    require("@tailwindcss/typography"), // 이 부분 추가
   ],
 };
