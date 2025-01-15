d4ad84a411467296b7f902039e4520f28f99cf10
ydw1996 <didekdnlt1996@gmail.com>
2025-01-15 21:37:36 +0900

    feat: Blog 레이아웃 구성

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,9 +9,9 @@ const BlogPage = async () => {
   const localPosts: Post[] = await getLocalPosts();
 
   return (
-    <div className="max-w-4xl mx-auto mt-12 px-5 md:px-0">
+    <div className="max-w-4xl mx-auto my-12 px-5 md:px-0">
       {/* 상단 Intro 섹션 */}
-      <div className="text-center mb-24">
+      <div className="text-center mb-12">
         <h1 className="text-4xl font-bold text-white mb-4">Dev Witty Blog</h1>
         <p className="text-gray-400 text-lg">
           도전과 창의성을 담은 위트있는 프론트엔드 개발 이야기
@@ -20,26 +20,22 @@ const BlogPage = async () => {
         {/* 태그 버튼 UI */}
         <Tags />
       </div>
-      <ul className="flex flex-col gap-8">
+
+      {/* 블로그 카드 목록 */}
+      <ul className="grid gap-10 sm:grid-cols-1 md:grid-cols-2">
         {localPosts.map(({ id, title, slug, date, description, thumbnailUrl }: Post) => (
           <Link key={id} href={`/blog/${slug}`}>
-            <li className="flex flex-col md:flex-row items-start gap-4">
+            <li className="bg-gray-800/30 backdrop-blur-lg rounded-2xl shadow-md overflow-hidden transition-shadow duration-200 hover:bg-primary-01/30 hover:shadow-[0_4px_15px_rgba(255,255,255,0.3)]">
               {/* 썸네일 이미지 */}
               {thumbnailUrl && (
-                <div className="w-32 h-32 flex-shrink-0">
-                  <Image
-                    width={150}
-                    height={150}
-                    src={thumbnailUrl}
-                    alt={title}
-                    className="object-cover w-28 h-28"
-                  />
+                <div className="relative w-full h-48">
+                  <Image src={thumbnailUrl} alt={title} layout="fill" className="object-cover" />
                 </div>
               )}
-              <div>
-                <p className="text-xl font-bold mb-2 hover:underline">{title}</p>
-                <span className="text-gray-500 text-sm">{date}</span>
-                <p className="text-gray-700 mt-2">{description}</p>
+              <div className="p-4">
+                <p className="text-lg font-bold text-white mb-2">{title}</p>
+                <p className="text-gray-500 text-sm mb-4">{date}</p>
+                <p className="text-gray-300 text-sm">{description}</p>
               </div>
             </li>
           </Link>
