9f6c2607bf86e34d42bb1c94cb7f618edc91126d
ydw1996 <didekdnlt1996@gmail.com>
2025-01-15 20:05:36 +0900

    feat: 메인 임시 레이아웃 추가 및 bgCanvas 수정

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -1,18 +1,15 @@
 export default function Intro() {
-    return (
-        <div></div>
-        // <section className="text-center sm:text-left py-20 px-8 bg-white from-white to-gray-100">
-        //     <div className="max-w-4xl mx-auto">
-        //         <h1 className="text-4xl font-bold text-blue-600 mb-4">My name is Dawit</h1>
-        //         <p className="text-2xl font-semibold text-black">
-        //             Just Do wit ! <span className="text-blue-500">And be witty.</span>
-        //         </p>
-        //         <div className="flex justify-center sm:justify-start gap-4 mt-8">
-        //             <button className="bg-black text-white px-6 py-2 rounded"></button>
-        //             <button className="bg-gray-300 px-6 py-2 rounded"></button>
-        //             <button className="bg-blue-500 text-white px-6 py-2 rounded"></button>
-        //         </div>
-        //     </div>
-        // </section>
-    );
+  return (
+    <div className="my-20 flex flex-col gap-32 max-w-4xl mx-auto">
+      <section className="w-[100%] bg-black/30 h-[500px] rounded-xl">
+        <div className="p-6 text-gray-500/50">section-01</div>
+      </section>
+      <section className="w-[100%] bg-black/30 h-[900px] rounded-xl">
+        <div className="p-6 text-gray-500/50">section-02</div>
+      </section>
+      <section className="w-[100%] bg-black/30 h-[900px] rounded-xl">
+        <div className="p-6 text-gray-500/50">section-03</div>
+      </section>
+    </div>
+  );
 }
